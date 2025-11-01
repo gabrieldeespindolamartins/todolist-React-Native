097236c99f319f042984b29b395f366f14fa398f
gabriel <gabriel14em@gamil.com>
2025-11-01 11:44:57 -0300

    separação de components e servições

---
+++ todolist/src/components/base.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native-web"
+
+const base = () => {
+  return (
+    <div>
+      <View>
+        <Text>separação de components e servições</Text>
+      </View>
+    </div>
+  )
+}
+
+export default base
---
+++ todolist/src/service/api.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+export default function Api() {
+  const [ tarefas, setTarefas ] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6903b880d0f10a340b257047.mockapi.io/tasks")
+      .then((res) => res.json())
+      .then((data) => setTarefas(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+      <ScrollView>
+        <h1>Tarefas</h1>
+        {tarefas.map((item) =>
+          <View key={item.taskId}> 
+            <text>{item.title}</text>
+            <text>{item.description}</text>
+            <text>{item.completed ? "Concluida" : "pendente"}</text>
+          </View>
+        )}
+      </ScrollView>
+  );
+}
